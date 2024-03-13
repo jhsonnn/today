97ab60ed1272e60698787054f804bb5fb60b15cc
hayeon <hayeon010904@gmail.com>
2024-03-14 02:11:50 +0900

    🐛

--- today/vite.config.ts
+++ today/vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true,
-  },
-  plugins: [mkcert()],
-});
+  plugins: [react()],
+})
