196c1e397490ffc82ed438354101a62623f49ac9
hayeon <hayeon010904@gmail.com>
2024-03-14 02:41:58 +0900

    🐛  충돌 해결 완..

--- today/src/App.tsx
+++ today/src/App.tsx
@@ -1,27 +1,22 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import DeleteModal from "./Components/DeleteModal/DeleteModal";
-import Header from "./Components/header/Header";
 
+import Header from "./Components/header/Header";
+import "./messaging-sw";
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
     </>
-  )
-}
+  );
+};
 
 function App() {
-
-  const { isOpen, id } = useDeleteModalStore();
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-
-
-      </Route>
+      <Route path="/" element={<Layout />}></Route>
     </Routes>
-  )
+  );
 }
 
 export default App;
