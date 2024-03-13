0ab8896d5755e7472299cd4308fc5240e6187807
deswaq1220 <deswaq789@naver.com>
2024-03-13 15:09:45 +0900

    ✨ 헤더

--- today/tailwind.config.js
+++ today/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        todayPink: "#ED7B9C",
+        todayPurple: "#847BED",
+        todayBlue: "#4E91FC",
+        todayNavy: "#3B475A",
+        todayOrange: "#FCAA430",
+        todayGreen: "#72C18E",
+        todayRed: "#F95F5F",
+      },
+    },
   },
   plugins: [],
 };
