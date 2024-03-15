8750d039ba5e5eedb7927a4f3df91010d8fcae1d
jhsonnn <jihyeongson13@gmail.com>
2024-03-15 19:46:37 +0900

    firebase.js 업데이트

--- today/firebase.js
+++ today/firebase.js
@@ -1,9 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import "firebase/compat/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-import "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +26,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
+export { addDoc, collection, db, getDocs, query, where };
 export default app;
